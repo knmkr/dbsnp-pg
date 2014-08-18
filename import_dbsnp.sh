5c9c0d579e811c4a912d65c43f6483c72fed73f3
Kensuke Numakura <knmkr3gma@gmail.com>
2014-08-18 17:46:14 +0900

    categorize bcp into minimal & optional

--- import_dbsnp.sh
+++ import_dbsnp.sh
@@ -3,7 +3,10 @@
 DBSNP_DB=$1
 DBSNP_USER=$2
 
-# for bcp in Allele RsMergeArch b141_SNPChrPosOnRef Population AlleleFreqBySsPop SNPSubSNPLink SnpChrCode; do
+minimal=(Allele RsMergeArch b141_SNPChrPosOnRef Population AlleleFreqBySsPop SNPSubSNPLink SnpChrCode)
+optional=(SnpChrCode SNPAlleleFreq)
+
+# for bcp in $minimal; do
 for bcp in SNPSubSNPLink; do
   echo "[INFO] Importing ${bcp}..."
   gunzip -c ${bcp}.bcp.gz | tr -d '\15' | nkf -Lu |  psql $DBSNP_DB $DBSNP_USER -c "COPY ${bcp} FROM stdin DELIMITERS '	' WITH NULL AS ''"
