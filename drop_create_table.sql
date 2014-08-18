05c51cd6cebe0669ffd9b24ffb73d1bc27d2eeb1
Kensuke Numakura <knmkr3gma@gmail.com>
2014-08-18 12:52:04 +0900

    add Allele table

--- drop_create_table.sql
+++ drop_create_table.sql
@@ -1,13 +1,52 @@
-DROP TABLE IF EXISTS SNPHistory;
-CREATE TABLE SNPHistory (
-       snp_id          integer  primary key,
-       create_time     timestamp,
-       last_updated_time        timestamp,
-       history_create_time      timestamp,
-       sometext1                 text,
-       sometext2                 text
-);
+-- ftp.ncbi.nih.gov:/snp/database/shared_schema/dbSNP_main_table.sql.gz
+--                                             /dbSNP_main_index.sql.gz
+--                                             /dbSNP_main_constraint.sql.gz
 
+-- CREATE TABLE [Allele]
+-- (
+-- [allele_id] [int] IDENTITY(1,1) NOT NULL ,
+-- [allele] [varchar](1024) NOT NULL ,
+-- [create_time] [smalldatetime] NOT NULL ,
+-- [rev_allele_id] [int] NULL ,
+-- [src] [varchar](10) NULL ,
+-- [last_updated_time] [smalldatetime] NULL ,
+-- [allele_left] [varchar](900) NULL
+-- )
+--
+-- CREATE NONCLUSTERED INDEX [i_rev_allele_id] ON [Allele] ([rev_allele_id] ASC)
+--
+-- ALTER TABLE [Allele] ADD CONSTRAINT [df_Allele_create_time] DEFAULT (GETDATE()) FOR [create_time]
+-- ALTER TABLE [Allele] ADD CONSTRAINT [df_Allele_update_time] DEFAULT (GETDATE()) FOR [last_updated_time]
+-- ALTER TABLE [Allele] ADD CONSTRAINT [pk_Allele]  PRIMARY KEY  CLUSTERED ([allele_id] ASC)
+DROP TABLE IF EXISTS Allele;
+CREATE TABLE Allele (
+       allele_id          serial         primary key,  -- 1,2,3,...
+       allele             varchar(1024)  not null,
+       create_time        timestamp      default CURRENT_TIMESTAMP,
+       rev_allele_id      integer,
+       src                varchar(10),
+       last_updated_time  timestamp      default CURRENT_TIMESTAMP,
+       allele_left        varchar(900)
+);
+CREATE INDEX i_rev_allele_id ON Allele (rev_allele_id);
+
+
+-- ftp.ncbi.nih.gov:/snp/organisms/human_9606/database/organism_schema/human_9606_table.sql.gz
+--                                                                    /human_9606_index.sql.gz
+--                                                                    /human_9606_constraint.sql.gz
+
+--
+-- DROP TABLE IF EXISTS SNPHistory;
+-- CREATE TABLE SNPHistory (
+--        snp_id          integer  primary key,
+--        create_time     timestamp,
+--        last_updated_time        timestamp,
+--        history_create_time      timestamp,
+--        sometext1                 text,
+--        sometext2                 text
+-- );
+
+--
 DROP TABLE IF EXISTS RsMergeArch;
 CREATE TABLE RsMergeArch (
        rsHigh            integer        primary key,
@@ -21,6 +60,7 @@ CREATE TABLE RsMergeArch (
        sometext1         text
 );
 
+--
 DROP TABLE IF EXISTS b141_SNPChrPosOnRef;
 CREATE TABLE b141_SNPChrPosOnRef (
        snp_id                    integer        primary key,
--- import_dbsnp.sh
+++ import_dbsnp.sh
@@ -3,7 +3,7 @@
 DBSNP_DB=$1
 DBSNP_USER=$2
 
-for bcp in RsMergeArch b141_SNPChrPosOnRef; do
+for bcp in Allele RsMergeArch b141_SNPChrPosOnRef; do
   echo "[INFO] Importing ${bcp}..."
   gunzip -c ${bcp}.bcp.gz | nkf -Lu | psql $DBSNP_DB $DBSNP_USER -c "COPY ${bcp} FROM stdin DELIMITERS '	' WITH NULL AS ''"
   psql $DBSNP_DB $DBSNP_USER -c "SELECT * FROM ${bcp} LIMIT 5"
