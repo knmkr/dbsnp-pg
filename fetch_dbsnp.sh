65cdcd36a10afac2c7531c514c9bee36175deec7
Kensuke Numakura <knmkr3gma@gmail.com>
2014-08-18 12:03:42 +0900

    add .bcp

--- fetch_dbsnp.sh
+++ fetch_dbsnp.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
 echo "[INFO] Fetching data..."
+# shared
+wget ftp.ncbi.nih.gov:/snp/database/shared_data/Allele.bcp.gz
+
+# rs
 wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/RsMergeArch.bcp.gz
 wget ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606/database/organism_data/b141_SNPChrPosOnRef.bcp.gz
+
+# ss <-> rs
+wget ftp.ncbi.nih.gov:/snp/organisms/human_9606/database/organism_data/SNPSubSNPLink.bcp.gz
+
+# allele freq by ss
+wget ftp.ncbi.nih.gov:/snp/organisms/human_9606/database/organism_data/AlleleFreqBySsPop.bcp.gz
+wget ftp.ncbi.nih.gov:/snp/organisms/human_9606/database/organism_data/Population.bcp.gz
+
 
 echo "[INFO] Done"
