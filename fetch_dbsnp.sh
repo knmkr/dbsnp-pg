c7f03106f110082466fe62dfcab8b5a333dec2dd
Kensuke Numakura <knmkr3gma@gmail.com>
2014-08-19 16:58:53 +0900

    update URLs of dbSNP FTP site

--- fetch_dbsnp.sh
+++ fetch_dbsnp.sh
@@ -2,18 +2,21 @@
 
 echo "[INFO] Fetching data..."
 # shared
-wget ftp.ncbi.nih.gov:/snp/database/shared_data/Allele.bcp.gz
+wget ftp.ncbi.nih.gov/snp/database/shared_data/Allele.bcp.gz
 
 # rs
 wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/RsMergeArch.bcp.gz
-wget ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606/database/organism_data/b141_SNPChrPosOnRef.bcp.gz
-
-# ss <-> rs
-wget ftp.ncbi.nih.gov:/snp/organisms/human_9606/database/organism_data/SNPSubSNPLink.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/b141_SNPChrPosOnRef.bcp.gz
 
 # allele freq by ss
-wget ftp.ncbi.nih.gov:/snp/organisms/human_9606/database/organism_data/AlleleFreqBySsPop.bcp.gz
-wget ftp.ncbi.nih.gov:/snp/organisms/human_9606/database/organism_data/Population.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/Population.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/AlleleFreqBySsPop.bcp.gz
 
+# ss <-> rs
+wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/SNPSubSNPLink.bcp.gz
+
+# optional
+# wget ftp.ncbi.nih.gov/snp/database/shared_data/SnpChrCode.bcp.gz
+# wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/SNPAlleleFreq.bcp.gz
 
 echo "[INFO] Done"
