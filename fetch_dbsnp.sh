79824ed8823e83fb5eab618230429ff8342781b1
Kensuke Numakura <knmkr3gma@gmail.com>
2014-08-26 16:18:56 +0900

    specify database (human_9606, human_9606_b141_GRCh37p13)

--- fetch_dbsnp.sh
+++ fetch_dbsnp.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
+database="human_9606"  # or `human_9606_b141_GRCh37p13`, etc.
+
 echo "[INFO] Fetching data..."
 # shared
 wget ftp.ncbi.nih.gov/snp/database/shared_data/Allele.bcp.gz
 
 # rs
-wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/RsMergeArch.bcp.gz
-wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/b141_SNPChrPosOnRef.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/RsMergeArch.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/b141_SNPChrPosOnRef.bcp.gz
 
 # allele freq by ss
-wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/Population.bcp.gz
-wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/AlleleFreqBySsPop.bcp.gz
-wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/dn_PopulationIndGrp.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/Population.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/AlleleFreqBySsPop.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/dn_PopulationIndGrp.bcp.gz
 
 # ss <-> rs
-wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/SNPSubSNPLink.bcp.gz
+wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/SNPSubSNPLink.bcp.gz
+
+# position on reference genome
+wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/b141_SNPChrPosOnRef.bcp.gz
 
 # optional
 # wget ftp.ncbi.nih.gov/snp/database/shared_data/SnpChrCode.bcp.gz
-# wget ftp.ncbi.nih.gov/snp/organisms/human_9606/database/organism_data/SNPAlleleFreq.bcp.gz
+# wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/SNPAlleleFreq.bcp.gz
 
 echo "[INFO] Done"
