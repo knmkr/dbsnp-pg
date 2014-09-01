cde782feb7d9fe30681d4b3a0b71fda722375a36
Kensuke Numakura <knmkr3gma@gmail.com>
2014-09-01 11:31:44 +0900

    add -c option to wget command

--- fetch_dbsnp.sh
+++ fetch_dbsnp.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 
-database="human_9606"  # or `human_9606_b141_GRCh37p13`, etc.
+database="human_9606"                   # GRCh38
+# database="human_9606_b141_GRCh37p13"  # GRCh37p13
 dbsnp="141"
 
 echo "[INFO] Fetching data..."
 # shared
-wget ftp.ncbi.nih.gov/snp/database/shared_data/Allele.bcp.gz
+wget -c ftp.ncbi.nih.gov/snp/database/shared_data/Allele.bcp.gz
 
 # rs
-wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/RsMergeArch.bcp.gz
+wget -c ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/RsMergeArch.bcp.gz
 
 # allele freq by ss
-wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/Population.bcp.gz
-wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/AlleleFreqBySsPop.bcp.gz
-wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/dn_PopulationIndGrp.bcp.gz
+wget -c ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/Population.bcp.gz
+wget -c ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/AlleleFreqBySsPop.bcp.gz
+wget -c ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/dn_PopulationIndGrp.bcp.gz
 
 # ss <-> rs
-wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/SNPSubSNPLink.bcp.gz
+wget -c ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/SNPSubSNPLink.bcp.gz
 
 # position on reference genome
-wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/b${dbsnp}_SNPChrPosOnRef.bcp.gz
+wget -c ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/b${dbsnp}_SNPChrPosOnRef.bcp.gz
 
 # optional
-# wget ftp.ncbi.nih.gov/snp/database/shared_data/SnpChrCode.bcp.gz
-# wget ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/SNPAlleleFreq.bcp.gz
+# wget -c ftp.ncbi.nih.gov/snp/database/shared_data/SnpChrCode.bcp.gz
+# wget -c ftp.ncbi.nih.gov/snp/organisms/${database}/database/organism_data/SNPAlleleFreq.bcp.gz
 
 echo "[INFO] Done"
