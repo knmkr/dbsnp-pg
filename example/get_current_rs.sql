4a544b46c526256aa1a55c51e280e871cc8b0818
Kensuke Numakura <knmkr3gma@gmail.com>
2014-09-01 11:24:36 +0900

    add results for SQL example

--- example/get_current_rs.sql
+++ example/get_current_rs.sql
@@ -3,3 +3,8 @@ SELECT
 FROM
     rsmergearch
 WHERE rshigh = ANY(ARRAY[330, 331, 332]);
+
+--  rshigh | rscurrent | orien2current
+-- --------+-----------+---------------
+--     332 | 121909001 |             0
+-- (1 row)
