6bb5cc743249230260d7f0ae3c4f3db2fe636d54
Kensuke Numakura <knmkr3gma@gmail.com>
2014-10-06 14:34:43 +0900

    add index for b141_SNPChrPosOnRef_GRCh37p13

---
+++ optional/04_add_constraints.sql
@@ -0,0 +1,2 @@
+CREATE UNIQUE INDEX _b141_SNPChrPosOnRef_GRCh37p13_ukey_rs ON _b141_SNPChrPosOnRef_GRCh37p13 (snp_id);
+CREATE INDEX _b141_SNPChrPosOnRef_GRCh37p13_chr_pos ON _b141_SNPChrPosOnRef_GRCh37p13 (chr, pos);
