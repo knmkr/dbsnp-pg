50af7d71b1753f91203e6c49c4044bf2620a89ca
Kensuke Numakura <knmkr3gma@gmail.com>
2014-10-06 15:48:59 +0900

    add rsList as argument in get-frequency query

--- optional/example/get_freq.sh
+++ optional/example/get_freq.sh
@@ -2,12 +2,13 @@
 
 PG_DB=$1
 PG_USER=$2
+RS_LIST=$3
 
 # Create tmp table for query rs list
 psql $PG_DB $PG_USER -f _drop_create_query_table.sql 1>/dev/null
 
 # Import query rs list
-awk '$1 ~ /^rs[0-9]+$/ {print $1}' rsList.txt| sed 's/rs//'| psql $PG_DB $PG_USER -c "COPY tmp_rs2current (rsHigh) FROM stdin"
+awk '$1 ~ /^rs[0-9]+/ {print $1}' $RS_LIST| sed 's/rs//'| psql $PG_DB $PG_USER -c "COPY tmp_rs2current (rsHigh) FROM stdin"
 
 # Update rs# to current rs#
 psql $PG_DB $PG_USER -f _update_to_current_rs.sql 1>/dev/null
