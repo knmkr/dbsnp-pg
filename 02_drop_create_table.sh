85634f0c39795f227f1d2a5fab9d6ee126fbd6ae
knmkr <knmkr3gma@gmail.com>
2015-05-03 23:28:47 +0900

    fix typo

--- 02_drop_create_table.sh
+++ 02_drop_create_table.sh
@@ -3,7 +3,7 @@
 PG_DB=$1
 PG_USER=$2
 
-psql $PG_DB $PG_USER -f ./postgresql/scheme/create_tables.sql -q
-psql $PG_DB $PG_USER -f ./postgresql/scheme/create_functions.sql -q
+psql $PG_DB $PG_USER -f ./postgresql/schema/create_tables.sql -q
+psql $PG_DB $PG_USER -f ./postgresql/schema/create_functions.sql -q
 
 echo "[INFO] Done"
