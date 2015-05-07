82addec94c3b0705726384b41d116cfea7436e07
knmkr <knmkr3gma@gmail.com>
2015-05-07 17:29:56 +0900

    rename file extensions of test data

--- test/test.sh
+++ test/test.sh
@@ -2,14 +2,17 @@
 
 test_db=test_dbsnp_$RANDOM
 
-echo    "[INFO] test_db: ${test_db}"
-echo    "[INFO] prepare for test..."
-echo -n "[INFO] "; dropdb --if-exists $test_db
-echo -n "[INFO] "; createdb $test_db
+echo "[INFO] test_db: ${test_db}"
+echo "[INFO] prepare for test..."
+dropdb --if-exists $test_db
+createdb $test_db
 
-cd ..;    ./02_drop_create_table.sh $test_db
-cd test; ../03_import_dbsnp.sh      $test_db
-cd ..;    ./04_add_constraints.sh   $test_db
+cd ..
+  ./02_drop_create_table.sh $test_db
+cd test
+  ../03_import_dbsnp.sh     $test_db
+cd ..
+  ./04_add_constraints.sh   $test_db
 
 echo -n "[INFO] "; type pg_prove
 if [ $? != 0 ]; then
@@ -17,17 +20,11 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-cd test; pg_prove -d $test_db .
+cd test
+  psql $test_db -c "CREATE EXTENSION IF NOT EXISTS pgtap;"
+  pg_prove -d $test_db .
 
-# echo "[INFO] test get_freq_by_rs.sql ..."
-# psql $test_db -f ../example/get_freq_by_rs.sql
+  echo "[INFO] clean up ${test_db} ..."
+  dropdb $test_db
 
-# #
-# psql $test_db -c "SELECT get_current_rs(332);"
-
-psql $test_db -c "\di"
-
-echo "[INFO] clean up ${test_db} ..."
-dropdb $test_db
-
-echo "[INFO] done"
+  echo "[INFO] done"
