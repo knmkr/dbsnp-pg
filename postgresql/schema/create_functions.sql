174f14dc1cbb76a2005d69ffafd6e7e66f753390
knmkr <knmkr3gma@gmail.com>
2015-05-07 23:13:01 +0900

    add function get_rs_by_pos()

--- postgresql/schema/create_functions.sql
+++ postgresql/schema/create_functions.sql
@@ -15,7 +15,17 @@ DECLARE
   ret record;
 BEGIN
   --
-  SELECT INTO ret chr, pos + 1 FROM b141_snpchrposonref_grch37p13 WHERE snp_id = _rs;
+  SELECT INTO ret chr, pos + 1 FROM b141_snpchrposonref_grch37p13 WHERE snp_id = _rs;  -- 0-based to 1-based
   RETURN ret;
+END
+$$ LANGUAGE plpgsql;
+
+-- FIXME: dependency on `b141_snpchrposonref_grch37p13`
+DROP FUNCTION IF EXISTS get_rs_by_pos(_chr varchar, _pos int);
+CREATE OR REPLACE FUNCTION get_rs_by_pos(_chr varchar, _pos int)
+RETURNS int[] AS $$
+BEGIN
+  --
+  RETURN (SELECT array_agg(snp_id) FROM b141_snpchrposonref_grch37p13 WHERE chr = _chr AND pos = _pos - 1);  -- 1-based to 0-based
 END
 $$ LANGUAGE plpgsql;
