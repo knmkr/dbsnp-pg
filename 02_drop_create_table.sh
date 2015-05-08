27bfc4e03830c8a117b3a93b65632d1a8712aa0d
knmkr <knmkr3gma@gmail.com>
2015-05-09 01:01:32 +0900

    add function get_json_freq_by_rs() for PostgreSQL >= 9.3

--- 02_drop_create_table.sh
+++ 02_drop_create_table.sh
@@ -6,4 +6,11 @@ PG_USER=$2
 psql $PG_DB $PG_USER -f ./postgresql/schema/create_tables.sql -q
 psql $PG_DB $PG_USER -f ./postgresql/schema/create_functions.sql -q
 
+server_version_num=$(psql $PG_DB $PG_USER -tc "SHOW server_version_num")  # e.g. 90300 (= PostgreSQL 9.3.0)
+
+if [ "$server_version_num" -ge 90300 ]; then
+  echo "[INFO] PostgreSQL >= 9.3"
+  psql $PG_DB $PG_USER -f ./postgresql/schema/create_functions_9.3.sql -q
+fi
+
 echo "[INFO] Done"
