c128cb25e9674ff7bbf3e28a81a1a224650e451b
Kensuke Numakura <knmkr3gma@gmail.com>
2015-07-16 17:07:39 +0900

    fix parser for multiple rsids

--- contrib/freq/script/vcf2tsv.py
+++ contrib/freq/script/vcf2tsv.py
@@ -3,40 +3,69 @@
 
 import sys
 
-pg_null = ''
-pg_true = 't'
+PG_NULL = ''
+PG_TRUE = 't'
 
-def mustache(s):
-    return '{' + s + '}'
+def mustache(record):
+    """
+    >>> mustache('abc')
+    '{abc}'
+    >>> mustache(['a', 'b', 'c'])
+    '{a,b,c}'
+    """
 
-for line in sys.stdin:
-    if line.startswith('#'):
-        continue
+    if type(record) == str:
+        return '{' + record + '}'
+    elif type(record) == list:
+        return '{' + ','.join(record) + '}'
 
-    vcf_records = line.split('\t')
+def value_by_index(values, indices):
+    """
+    >>> value_by_index(['a', 'b', 'c'], [0, 2])
+    ['a', 'c']
+    """
 
-    chrom, pos, rsids, ref, alt = vcf_records[0:5]
-    infos_raw = vcf_records[7]
+    return [value for i,value in enumerate(values) if i in indices]
 
-    infos_splitted = (info.split('=') if len(info.split('=')) == 2 else [info, pg_true] for info in infos_raw.rstrip(';').split(';'))
-    infos = dict((k.strip(), v.strip()) for k,v in infos_splitted)
-
-    for i,rsid in enumerate(rsids.split(';')):
-        if rsid.startswith('rs'):
-            rs = rsid.replace('rs', '')
-        elif rsid == '.':
-            rs = pg_null
-        else:
+def _main():
+    for line in sys.stdin:
+        if line.startswith('#'):
             continue
 
-        # CHROM    POS    ID    REF    ALT    INFO_EAS_AF    INFO_EUR_AF    INFO_AFR_AF    INFO_AMR_AF    INFO_SAS_AF
-        print '\t'.join([chrom,
-                         pos,
-                         rs,
-                         ref,
-                         mustache(alt.split(',')[i]),
-                         mustache(infos.get('EAS_AF', pg_null).split(',')[i]),
-                         mustache(infos.get('EUR_AF', pg_null).split(',')[i]),
-                         mustache(infos.get('AFR_AF', pg_null).split(',')[i]),
-                         mustache(infos.get('AMR_AF', pg_null).split(',')[i]),
-                         mustache(infos.get('SAS_AF', pg_null).split(',')[i])])
+        vcf_records = line.split('\t')
+
+        chrom, pos, rsids, ref, alt = vcf_records[0:5]
+        infos_raw = vcf_records[7]
+
+        infos_splitted = (info.split('=') if len(info.split('=')) == 2 else [info, PG_TRUE] for info in infos_raw.rstrip(';').split(';'))
+        infos = dict((k.strip(), v.strip()) for k,v in infos_splitted)
+
+        rsids = rsids.split(';')
+        rsid_indices = dict()
+        for uniq_rsid in set(rsids):
+            rsid_indices[uniq_rsid] = [i for i,rsid in enumerate(rsids) if rsid == uniq_rsid]
+
+        for rsid, rsid_index in rsid_indices.items():
+            if rsid.startswith('rs'):
+                rs = rsid.replace('rs', '')
+            elif rsid == '.':
+                rs = PG_NULL
+            else:
+                continue
+
+            # CHROM	POS	ID	REF	ALT	INFO_EAS_AF	INFO_EUR_AF	INFO_AFR_AF	INFO_AMR_AF	INFO_SAS_AF
+            print '\t'.join([chrom,
+                             pos,
+                             rs,
+                             ref,
+                             mustache(value_by_index(alt.split(','), rsid_index)),
+                             mustache(value_by_index(infos.get('EAS_AF', PG_NULL).split(','), rsid_index)),
+                             mustache(value_by_index(infos.get('EUR_AF', PG_NULL).split(','), rsid_index)),
+                             mustache(value_by_index(infos.get('AFR_AF', PG_NULL).split(','), rsid_index)),
+                             mustache(value_by_index(infos.get('AMR_AF', PG_NULL).split(','), rsid_index)),
+                             mustache(value_by_index(infos.get('SAS_AF', PG_NULL).split(','), rsid_index))])
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    _main()
--- contrib/freq/test/test_public.get_tbl_allele_freq_by_rs.sql
+++ contrib/freq/test/test_public.get_tbl_allele_freq_by_rs.sql
@@ -1,10 +1,23 @@
 BEGIN;
-SELECT * FROM plan(1);
+SELECT * FROM plan(3);
 
 SELECT set_eq(
   'SELECT snp_id,ref,alt,freq_eas FROM allelefreqin1000genomesphase3_b37 WHERE snp_id = 367896724',
   $$ VALUES
   (367896724,'A'::varchar,'{AC}'::varchar[],'{0.3363}'::real[])
+  $$
+);
+
+SELECT set_eq(
+  'SELECT chr,pos,snp_id,ref,alt,freq_eas FROM allelefreqin1000genomesphase3_b37 WHERE snp_id = 1',
+  $$ VALUES
+  ('X'::varchar,1,1,'G'::varchar,'{A,C}'::varchar[],'{0.2,0.5}'::real[])
+  $$
+);
+SELECT set_eq(
+  'SELECT chr,pos,snp_id,ref,alt,freq_eas FROM allelefreqin1000genomesphase3_b37 WHERE snp_id = 2',
+  $$ VALUES
+  ('X'::varchar,1,2,'G'::varchar,'{T}'::varchar[],'{0.3}'::real[])
   $$
 );
 
