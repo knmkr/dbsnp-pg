60224aa13c7acf06668344c0307ba552507e2c7e
Kensuke Numakura <knmkr3gma@gmail.com>
2015-08-27 16:06:12 +0900

    change to use `allele` instead of `ref` and `alt`

--- contrib/freq/test/test_public.get_tbl_allele_freq_by_rs_history.sql
+++ contrib/freq/test/test_public.get_tbl_allele_freq_by_rs_history.sql
@@ -2,13 +2,13 @@ BEGIN;
 SELECT * FROM plan(1);
 
 SELECT set_eq(
-  'SELECT * FROM get_tbl_allele_freq_by_rs_history(ARRAY[671, 2230021, 4134524, 4986830, 60823674])',
+  'SELECT snp_id,snp_current,allele,freq::text FROM get_tbl_allele_freq_by_rs_history(ARRAY[671, 2230021, 4134524, 4986830, 60823674])',
   $$ VALUES
-  (671,      671, '{G}'::varchar[],'{A}'::varchar[],'{0.1736}'::real[]),
-  (2230021,  671, '{G}'::varchar[],'{A}'::varchar[],'{0.1736}'::real[]),
-  (4134524,  671, '{G}'::varchar[],'{A}'::varchar[],'{0.1736}'::real[]),
-  (4986830,  671, '{G}'::varchar[],'{A}'::varchar[],'{0.1736}'::real[]),
-  (60823674, 671, '{G}'::varchar[],'{A}'::varchar[],'{0.1736}'::real[])
+  (671,      671, '{G,A}'::varchar[], '{0.799517,0.200483}'),
+  (2230021,  671, '{G,A}'::varchar[], '{0.799517,0.200483}'),
+  (4134524,  671, '{G,A}'::varchar[], '{0.799517,0.200483}'),
+  (4986830,  671, '{G,A}'::varchar[], '{0.799517,0.200483}'),
+  (60823674, 671, '{G,A}'::varchar[], '{0.799517,0.200483}')
   $$
 );
 
