777c0a08e527452d6b875c325a996939ed5e60a9
Kensuke Numakura <knmkr3gma@gmail.com>
2015-08-27 16:29:14 +0900

    change to use `allele` instead of `ref` and `alt`

--- contrib/freq/postgresql/example/get_allele_freq_by_rs.sql
+++ contrib/freq/postgresql/example/get_allele_freq_by_rs.sql
@@ -1,6 +1,11 @@
 SELECT
-    snp_id, ref, alt, freq_eas
+    *
 FROM
     allelefreqin1000genomesphase3_b37
 WHERE
-    snp_id = 62636497;
+    snp_id = 671;
+
+--  snp_id | chr |    pos    | allele |   freq
+-- --------+-----+-----------+--------+----------
+--     671 | 12  | 112241766 | A      | 0.200483
+--     671 | 12  | 112241766 | G      | 0.799517
