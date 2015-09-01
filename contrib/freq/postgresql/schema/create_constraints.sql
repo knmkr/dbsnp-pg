56b90a379c57289795bfab6f84d321a973abca0e
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-01 14:25:47 +0900

    wip: add phase1 as another table

--- contrib/freq/postgresql/schema/create_constraints.sql
+++ contrib/freq/postgresql/schema/create_constraints.sql
@@ -1,3 +1,6 @@
--- AlleleFreqIn1000GenomesPhase3_b37
-DROP INDEX IF EXISTS AlleleFreqIn1000GenomesPhase3_ukey_snp_id_allele;
-CREATE UNIQUE INDEX AlleleFreqIn1000GenomesPhase3_ukey_snp_id_allele ON AlleleFreqIn1000GenomesPhase3_b37 (snp_id, allele) WHERE snp_id IS NOT NULL;
+-- -- AlleleFreqIn1000GenomesPhase3_b37
+-- DROP INDEX IF EXISTS AlleleFreqIn1000GenomesPhase3_ukey_snp_id_allele;
+-- CREATE UNIQUE INDEX AlleleFreqIn1000GenomesPhase3_ukey_snp_id_allele ON AlleleFreqIn1000GenomesPhase3_b37 (snp_id, allele) WHERE snp_id IS NOT NULL;
+
+DROP INDEX IF EXISTS AlleleFreqIn1000GenomesPhase1_ukey_snp_id_allele;
+CREATE UNIQUE INDEX AlleleFreqIn1000GenomesPhase1_ukey_snp_id_allele ON AlleleFreqIn1000GenomesPhase1_b37 (snp_id, allele);
--- contrib/freq/postgresql/schema/create_tables.sql
+++ contrib/freq/postgresql/schema/create_tables.sql
@@ -1,7 +1,16 @@
---
-DROP TABLE IF EXISTS AlleleFreqIn1000GenomesPhase3_b37;
-CREATE TABLE AlleleFreqIn1000GenomesPhase3_b37 (
-       snp_id    integer,
+-- --
+-- DROP TABLE IF EXISTS AlleleFreqIn1000GenomesPhase3_b37;
+-- CREATE TABLE AlleleFreqIn1000GenomesPhase3_b37 (
+--        snp_id    integer,
+--        chr       varchar(32)    not null,
+--        pos       integer        not null,
+--        allele    varchar(1024)  not null,
+--        freq      real           not null
+-- );
+
+DROP TABLE IF EXISTS AlleleFreqIn1000GenomesPhase1_b37;
+CREATE TABLE AlleleFreqIn1000GenomesPhase1_b37 (
+       snp_id    integer        not null,
        chr       varchar(32)    not null,
        pos       integer        not null,
        allele    varchar(1024)  not null,
