cfaddfc5271ff850cb2ca8eae8b373cdb41e1d66
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-05 18:33:26 +0900

    fix source_id in test

--- contrib/freq/test/test_public.get_tbl_allele_freq_by_rs_history.sql
+++ contrib/freq/test/test_public.get_tbl_allele_freq_by_rs_history.sql
@@ -2,7 +2,15 @@ BEGIN;
 SELECT * FROM plan(2);
 
 SELECT set_eq(
-  'SELECT snp_id,snp_current,allele,freq::text FROM get_tbl_allele_freq_by_rs_history(1, ARRAY[671, 2230021, 4134524, 4986830, 60823674])',
+  'SELECT snp_id,snp_current,allele,freq::text FROM get_tbl_allele_freq_by_rs_history(1, ARRAY[671,26])',
+  $$ VALUES
+  (671,      671, '{G,A}'::varchar[], '{0.7832,0.2168}'),
+  (26,        26, '{C,T}'::varchar[], '{0.6836,0.3164}')
+  $$
+);
+
+SELECT set_eq(
+  'SELECT snp_id,snp_current,allele,freq::text FROM get_tbl_allele_freq_by_rs_history(2, ARRAY[671, 2230021, 4134524, 4986830, 60823674])',
   $$ VALUES
   (671,      671, '{G,A}'::varchar[], '{0.7995,0.2005}'),
   (2230021,  671, '{G,A}'::varchar[], '{0.7995,0.2005}'),
@@ -11,15 +19,6 @@ SELECT set_eq(
   (60823674, 671, '{G,A}'::varchar[], '{0.7995,0.2005}')
   $$
 );
-
-SELECT set_eq(
-  'SELECT snp_id,snp_current,allele,freq::text FROM get_tbl_allele_freq_by_rs_history(2, ARRAY[671,26])',
-  $$ VALUES
-  (671,      671, '{G,A}'::varchar[], '{0.7832,0.2168}'),
-  (26,        26, '{C,T}'::varchar[], '{0.6836,0.3164}')
-  $$
-);
-
 
 SELECT * FROM finish();
 ROLLBACK;
