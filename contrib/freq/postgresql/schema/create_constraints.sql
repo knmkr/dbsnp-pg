320ef7a4d65195c2273a23064a5d2dffad7509c9
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-05 18:32:20 +0900

    remove unused index

--- contrib/freq/postgresql/schema/create_constraints.sql
+++ contrib/freq/postgresql/schema/create_constraints.sql
@@ -3,6 +3,3 @@ CREATE UNIQUE INDEX allelefreq_1_ukey_snp_id_allele ON AlleleFreq_1 (snp_id, all
 
 DROP INDEX IF EXISTS allelefreq_2_ukey_snp_id_allele;
 CREATE UNIQUE INDEX allelefreq_2_ukey_snp_id_allele ON AlleleFreq_2 (snp_id, allele);
-
-CREATE INDEX idx_allelefreq_1 ON AlleleFreq_1 (snp_id);
-CREATE INDEX idx_allelefreq_2 ON AlleleFreq_2 (snp_id);
