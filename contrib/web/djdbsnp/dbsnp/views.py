bd6826e5d8b14c815cfecefa3aca7c1866458876
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-06 18:04:17 +0900

    add TODOs

--- contrib/web/djdbsnp/dbsnp/views.py
+++ contrib/web/djdbsnp/dbsnp/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render
 from .models import SNP
 
 def index(request):
-    #
+    # TODO: dbSNP build
+
     rs = str(671)
     chrpos = SNP.get_pos_by_rs(rs)
+    # TODO: chrom, pos on b37, b38
+
+    # TODO: gene
+
+    # TODO: reference sequence
+
+    # TODO: snp fasta sequence
+
+    # TODO: rs id history
+    # - rshigh, rslow, rscurrent
+
+    # TODO: freq
+    # - alleles, freq, source
+    # - EAS, EUR, AFR
+
+    # TODO: OMIM
+
+    # TODO: LD
+
+    # TODO: GWAS
 
     return render(request, 'index.html', dict(rs=rs, chrpos=chrpos))
