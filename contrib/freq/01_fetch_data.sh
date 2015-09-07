933b3c795f5c6c152b902b9344a30e712753a6bc
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-07 14:44:43 +0900

    add 1000 genomes phase 1 in downlaod script

--- contrib/freq/01_fetch_data.sh
+++ contrib/freq/01_fetch_data.sh
@@ -7,11 +7,12 @@ cd ${DATA_DIR}
 
 echo "[contrib/freq] [INFO] Fetching data..."
 
-# GRCh37
+# 1000 genomes phase1
+wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20110521/ALL.chr{{1..22},X}.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz
+
+# 1000 genomes phase3
 wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr{1..22}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
 wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf.gz
 wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrY.phase3_integrated_v1b.20130502.genotypes.vcf.gz
-
-# TODO: GRCh38
 
 echo "[contrib/freq] [INFO] Done"
