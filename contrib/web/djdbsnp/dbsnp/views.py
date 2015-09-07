d11bc6f92111b7664a234e29fb1c9c5e7201c166
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-07 10:58:24 +0900

    add json response and tsv download

--- contrib/web/djdbsnp/dbsnp/views.py
+++ contrib/web/djdbsnp/dbsnp/views.py
@@ -1,6 +1,9 @@
+import csv
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 from django.conf import settings
 from .models import SNP
+
 
 def index(request):
     rs = str(671)
@@ -25,10 +28,24 @@ def index(request):
 
     # TODO: GWAS
 
-    return render(request, 'index.html',
-                  {'dbsnp_build': settings.DBSNP_BUILD,
-                   'dbsnp_ref_genome_build': settings.DBSNP_REF_GENOME_BUILD,
-                   'rs': rs,
-                   'chrom': chrom,
-                   'pos': pos,
-                   'rs_current': rs_current})
+    records = {'dbsnp_build': settings.DBSNP_BUILD,
+               'dbsnp_ref_genome_build': settings.DBSNP_REF_GENOME_BUILD,
+               'rs': rs,
+               'chrom': chrom,
+               'pos': pos,
+               'rs_current': rs_current}
+
+    fmt = request.GET.get('fmt', '')
+
+    if fmt == 'json':
+        response = JsonResponse(records)
+        return response
+    elif fmt == 'tsv':
+        response = HttpResponse(content_type='text/tab-separated-values')
+        response['Content-Disposition'] = 'attachment; filename="{}.tsv"'.format('rs' + rs)
+        writer = csv.DictWriter(response, fieldnames=records.keys(), delimiter='\t')
+        writer.writeheader()
+        writer.writerow(records)
+        return response
+    else:
+        return render(request, 'index.html', records)
