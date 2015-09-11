130ead112fb4fb6bd2ed5a84898ea5dd8759d340
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-11 14:48:12 +0900

    wip: get fasta seq for REF

--- contrib/freq/script/hapmap.phased2vcf.py
+++ contrib/freq/script/hapmap.phased2vcf.py
@@ -8,11 +8,11 @@ import gzip
 import csv
 import re
 
-# from pyfasta import Fasta
+from pyfasta import Fasta
 from vcf.writer import VCFWriter
 
 def get_ref_allele(fasta, chrom, pos):
-    return fasta.sequence({'chr': str(chrom), 'start': int(pos), 'stop': int(pos)}, one_based=True)
+    return fasta.sequence({'chr': str(chrom), 'start': int(pos), 'stop': int(pos)}, one_based=True).upper()
 
 def chromosomes(key, regexp):
     if regexp.search(key):
@@ -24,13 +24,13 @@ def chromosomes(key, regexp):
 def _main():
     parser = argparse.ArgumentParser()
     parser.add_argument('hapmap')
-    # parser.add_argument('--fa', requreid=True)
+    parser.add_argument('--fa', required=True)
     args = parser.parse_args()
 
     fin = gzip.open(args.hapmap, 'rb') if os.path.splitext(args.hapmap)[1] == '.gz' else open(args.hapmap)
     chrom = re.findall(r'\.chr(.*)_', args.hapmap)[0]
 
-    # fa = Fasta(args.fa, key_fn=lambda key: chromosomes(key, re.compile('chr([0-9XY]+)')))
+    fa = Fasta(args.fa, key_fn=lambda key: chromosomes(key, re.compile('chr([0-9XY]+)')))
 
     reader = csv.DictReader(fin, delimiter=' ')
     sample_ids = sorted(list(set([x.strip('_A').strip('_B') for x in reader.fieldnames if x.endswith('_A') or x.endswith('_B')])))
@@ -48,15 +48,14 @@ def _main():
         row['POS'] = record['position_b36']
         row['ID'] = record['rsID']
 
-        # ref = get_ref_allele(fa, chrom, record['position_b36'])
-        ref = 'N'
+        ref = get_ref_allele(fa, chrom, record['position_b36'])
         alts = []
 
         for sample_id in sample_ids:
             alts.append(record[sample_id + '_A'])
             alts.append(record[sample_id + '_B'])
 
-        # assert ref in alts
+        assert ref in alts, (row, ref, set(alts))
         alts = list(set(alts) - set([ref]))
         alleles = [ref] + alts
 
