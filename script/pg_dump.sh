729a0fa588ae8c25fcd133126a0e98dde0ef11c9
Kensuke Numakura <knmkr3gma@gmail.com>
2015-10-09 18:30:54 +0900

    rename dump file

--- script/pg_dump.sh
+++ script/pg_dump.sh
@@ -3,8 +3,8 @@
 PG_DB=$1
 PG_USER=$2
 TAG=$3
-NUM_JOB=$4
+# NUM_JOB=$4
 
-pg_dump -Fc -T '(allelefreq*|gwascatalog*)' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.pg_dump
-pg_dump -Fc -t 'allelefreq*' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.freq.pg_dump
-pg_dump -Fc -t 'gwascatalog*' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.gwascatalog.pg_dump
+pg_dump -Fc -T '(allelefreq*|gwascatalog*)' $PG_DB -U $PG_USER > dbsnp-pg-min-${TAG}-${PG_DB}.pg_dump
+pg_dump -Fc -t 'allelefreq*' $PG_DB -U $PG_USER > dbsnp-pg-min-${TAG}-${PG_DB}.freq.pg_dump
+pg_dump -Fc -t 'gwascatalog*' $PG_DB -U $PG_USER > dbsnp-pg-min-${TAG}-${PG_DB}.gwascatalog.pg_dump
