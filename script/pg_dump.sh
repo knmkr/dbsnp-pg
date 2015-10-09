89aa512775c20eaeca0545d006d6f394e80c9fa6
Kensuke Numakura <knmkr3gma@gmail.com>
2015-10-09 18:05:42 +0900

    update pg_dump/restore

--- script/pg_dump.sh
+++ script/pg_dump.sh
@@ -5,8 +5,6 @@ PG_USER=$2
 TAG=$3
 NUM_JOB=$4
 
-# pg_dump -Fd -f ${PG_DB}-${TAG} -j ${NUM_JOB} ${PG_DB} -U ${PG_USER}
-
-pg_dump -T '(allelefreq*|gwascatalog*)' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.core
-pg_dump -t 'allelefreq*' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.freq
-pg_dump -t 'gwascatalog*' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.gwascatalog
+pg_dump -Fc -T '(allelefreq*|gwascatalog*)' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.pg_dump
+pg_dump -Fc -t 'allelefreq*' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.freq.pg_dump
+pg_dump -Fc -t 'gwascatalog*' $PG_DB -U $PG_USER > ${PG_DB}-${TAG}.gwascatalog.pg_dump
--- script/pg_restore.sh
+++ script/pg_restore.sh
@@ -5,8 +5,15 @@ PG_USER=$2
 TAG=$3
 NUM_JOB=$4
 
+BASE_URL=https://github.com/knmkr/dbsnp-pg-min/releases/download
+TARGET=dbsnp_b144_GRCh37
+
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetchging data ..."
-# wget -c https://github.com/knmkr/dbsnp-pg-min/releases/download/${TAG}/dbsnp-pg-min-${TAG}_b144_GRCh37.pg_dump.sql.gz
+wget -c ${BASE_URL}/${TAG}/${TARGET}-${TAG}.pg_dump
+wget -c ${BASE_URL}/${TAG}/${TARGET}-${TAG}.freq.pg_dump
+wget -c ${BASE_URL}/${TAG}/${TARGET}-${TAG}.gwascatalog.pg_dump
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_restore ..."
-pg_restore -d ${PG_DB} -U ${PG_USER} -j ${NUM_JOB}
+for fin in ${TARGET}-${TAG}*; do
+    pg_restore -d ${PG_DB} -U ${PG_USER} -j ${NUM_JOB} ${fin}
+done
