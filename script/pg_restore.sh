02237ce2d9dd9aca3bbbf47b2ca7da68f20e55e2
Kensuke Numakura <knmkr3gma@gmail.com>
2015-10-09 18:48:55 +0900

    fix dump filename

--- script/pg_restore.sh
+++ script/pg_restore.sh
@@ -6,12 +6,12 @@ TAG=$3
 NUM_JOB=$4
 
 BASE_URL=https://github.com/knmkr/dbsnp-pg-min/releases/download
-TARGET=dbsnp_b144_GRCh37
+TARGET=dbsnp-pg-min-${TAG}-b144-GRCh37  # FIXME: build versions
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetchging data ..."
-wget -c ${BASE_URL}/${TAG}/${TARGET}-${TAG}.pg_dump
-wget -c ${BASE_URL}/${TAG}/${TARGET}-${TAG}.freq.pg_dump
-wget -c ${BASE_URL}/${TAG}/${TARGET}-${TAG}.gwascatalog.pg_dump
+wget -c ${BASE_URL}/${TAG}/${TARGET}.pg_dump
+wget -c ${BASE_URL}/${TAG}/${TARGET}.freq.pg_dump
+wget -c ${BASE_URL}/${TAG}/${TARGET}.gwascatalog.pg_dump
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_restore ..."
 for fin in ${TARGET}-${TAG}*; do
