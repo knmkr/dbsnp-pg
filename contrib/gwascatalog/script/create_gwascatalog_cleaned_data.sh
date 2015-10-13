0db123eccb7ddead10e8e599e1a787d98bd39eaf
Kensuke Numakura <knmkr3gma@gmail.com>
2015-10-13 16:30:45 +0900

    fix export data of gwascatalog cleaned

--- contrib/gwascatalog/script/create_gwascatalog_cleaned_data.sh
+++ contrib/gwascatalog/script/create_gwascatalog_cleaned_data.sh
@@ -3,7 +3,7 @@
 PG_DB=$1
 PG_USER=$2
 
-dst=gwascatalog_cleaned_$(date +"%Y-%m-%d").tsv
+dst=gwascatalog.cleaned-$(date +"%Y-%m-%d").tsv
 
 mkdir -p tmp
 cd tmp
@@ -11,7 +11,7 @@ rm -f ${dst}
 
 echo "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Creating ${dst} ..."
 
-psql ${PG_DB} ${PG_USER} -c "\COPY (SELECT * FROM gwascatalog WHERE snp_id IS NOT NULL) TO STDOUT DELIMITER '	' CSV HEADER " > ${dst}
+psql ${PG_DB} ${PG_USER} -c "\COPY (SELECT * FROM gwascatalog) TO STDOUT DELIMITER '	' CSV HEADER " > ${dst}
 printf "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` #records: "; wc -l ${dst}
 
 mv ${dst} ../
