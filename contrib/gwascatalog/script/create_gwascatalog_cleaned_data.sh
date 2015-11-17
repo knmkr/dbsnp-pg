fada7471aff8534867867772051718b8547eae65
Kensuke Numakura <knmkr3gma@gmail.com>
2015-11-17 18:24:33 +0900

    remove tmp dir

--- contrib/gwascatalog/script/create_gwascatalog_cleaned_data.sh
+++ contrib/gwascatalog/script/create_gwascatalog_cleaned_data.sh
@@ -5,14 +5,9 @@ PG_USER=$2
 
 dst=gwascatalog-cleaned-$(date +"%Y-%m-%d").tsv
 
-mkdir -p tmp
-cd tmp
-
 echo "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Creating ${dst} ..."
 
 psql ${PG_DB} ${PG_USER} -c "\COPY (SELECT * FROM gwascatalog) TO STDOUT DELIMITER '	' CSV HEADER " > ${dst}
 printf "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` #records: "; wc -l ${dst}
-
-mv ${dst} ../
 
 echo "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
