12f00993065c8f3884b2c9834a777af6e9fe1f37
Kensuke Numakura <knmkr3gma@gmail.com>
2016-02-24 18:38:18 +0900

    add --keep plink option. update vcf snp_id to current. set CHB,JPT,CHS as East Asian default

--- contrib/freq/03_import_data.sh
+++ contrib/freq/03_import_data.sh
@@ -9,26 +9,24 @@ FRQ_TOOL=$5
 # Set defaults
 : ${FRQ_TOOL:=vcftools}
 
-source_ids=(2 100 200 300)
+source_ids=(3 4 100 200 300)
 
 # TODO: Avoid hardcoding source_ids
-declare -A target2filename=( \
+declare -A source_id2filename=( \
   ["1"]="1000genomes.phase1/ALL.chr*.*.vcf.gz"
   ["2"]="1000genomes.phase3/ALL.chr*.*.vcf.gz"
   ["3"]="1000genomes.phase1/ALL.chr*.*.vcf.gz"
   ["4"]="1000genomes.phase3/ALL.chr*.*.vcf.gz"
-  ["6"]="1000genomes.phase3/ALL.chr*.*.vcf.gz"
   ["100"]="1000genomes.phase3/ALL.chr*.*.vcf.gz"
   ["200"]="1000genomes.phase3/ALL.chr*.*.vcf.gz"
   ["300"]="1000genomes.phase3/ALL.chr*.*.vcf.gz"
 )
 
-declare -A target2sample_ids=( \
-  ["1"]="sample_ids.1000genomes.phase1.CHB+JPT.txt"
-  ["2"]="sample_ids.1000genomes.phase3.CHB+JPT.txt"
+declare -A source_id2sample_ids=( \
+  ["1"]="sample_ids.1000genomes.phase1.JPT.txt"
+  ["2"]="sample_ids.1000genomes.phase3.JPT.txt"
   ["3"]="sample_ids.1000genomes.phase1.CHB+JPT+CHS.txt"
   ["4"]="sample_ids.1000genomes.phase3.CHB+JPT+CHS.txt"
-  ["6"]="sample_ids.1000genomes.phase3.JPT.txt"
   ["100"]="sample_ids.1000genomes.phase3.CEU.no-pat-mat.txt"
   ["200"]="sample_ids.1000genomes.phase3.YRI.no-pat-mat.txt"
   ["300"]="sample_ids.1000genomes.phase3.no-pat-mat.txt"
@@ -54,41 +52,51 @@ else
     exit 1
 fi
 
-for target in ${source_ids[@]}; do
-    echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Target sample ids: ${target2sample_ids[${target}]}"
+for source_id in ${source_ids[@]}; do
+    echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Target sample ids: ${source_id2sample_ids[${source_id}]}"
     echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Drop index if exists ..."
-    psql $PG_DB $PG_USER -c "DROP INDEX IF EXISTS allelefreq_${target}_snp_id" -q
+    psql $PG_DB $PG_USER -c "DROP INDEX IF EXISTS allelefreq_${source_id}_snp_id" -q
 
-    for filename in ${target2filename[${target}]}; do
+    for filename in ${source_id2filename[${source_id}]}; do
         echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` ${filename}"
+
+        keep_ids=${BASE_DIR}/script/${source_id2sample_ids[${source_id}]}
 
         if [ "${FRQ_TOOL}" = "vcftools" ]; then
             # vcftools
             echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Calculating freq ..."
-            vcftools --freq --out ${filename}.${target}.frq --keep ${BASE_DIR}/script/${target2sample_ids[${target}]} --gzvcf ${filename}
+            vcftools --freq --out ${filename}.${source_id}.frq --keep ${keep_ids} --gzvcf ${filename}
 
             echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Formatting ..."
-            paste <(gzip -dc ${filename}| grep -v '##'| cut -f 3| tail -n+2) <(cut -f 5- ${filename}.${target}.frq.frq| tail -n+2| ${py} ${BASE_DIR}/script/frq2pg_array.py)| \
-                ${py} ${BASE_DIR}/script/filter.py --source-id ${target} > ${filename}.${target}.frq.frq.csv
+            paste <(gzip -dc ${filename}| grep -v '##'| cut -f 3| tail -n+2) <(cut -f 5- ${filename}.${source_id}.frq.frq| tail -n+2| ${py} ${BASE_DIR}/script/frq2pg_array.py)| \
+                ${py} ${BASE_DIR}/script/filter.py --source-id ${source_id} > ${filename}.${source_id}.frq.frq.csv
 
         elif [ "${FRQ_TOOL}" = "plink" ]; then
             # plink
             echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Calculating freq ..."
-            plink --vcf ${filename} --make-bed --out tmp
-            plink --bfile tmp --freq --out ${filename}.${target}.frq
-            rm -f tmp.{bed,fam,log,bim,nosex} ${filename}.${target}.{log,nosex}
+            awk '{print $1,$1}' ${keep_ids} > tmp.keep_ids.fam
+            plink --vcf ${filename} --make-bed --keep tmp.keep_ids.fam --out tmp
+            plink --bfile tmp --freq --out ${filename}.${source_id}.frq
+            rm -f tmp.{bed,fam,log,bim,nosex,keep_ids.fam} ${filename}.${source_id}.{log,nosex}
 
             echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Formatting ..."
-            cat ${filename}.${target}.frq.frq| ${py} ${BASE_DIR}/script/plinkfrq2pg_array.py| \
-                ${py} ${BASE_DIR}/script/filter.py --source-id ${target} > ${filename}.${target}.frq.frq.csv
+            cat ${filename}.${source_id}.frq.frq| ${py} ${BASE_DIR}/script/plinkfrq2pg_array.py| \
+                ${py} ${BASE_DIR}/script/filter.py --source-id ${source_id} > ${filename}.${source_id}.frq.frq.csv
         fi
 
         echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Importing ..."
-        cat ${filename}.${target}.frq.frq.csv| psql $PG_DB $PG_USER -c "COPY AlleleFreq FROM stdin DELIMITERS '	' WITH NULL AS ''" -q
+        cat ${filename}.${source_id}.frq.frq.csv| psql $PG_DB $PG_USER -c "COPY AlleleFreq FROM stdin DELIMITERS '	' WITH NULL AS ''" -q
     done;
 
     echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Creating constraints ..."
-    psql $PG_DB $PG_USER -c "CREATE INDEX allelefreq_${target}_snp_id ON AlleleFreq_${target} (snp_id)" -q
+    psql $PG_DB $PG_USER -c "CREATE INDEX allelefreq_${source_id}_snp_id ON AlleleFreq_${source_id} (snp_id)" -q
+
+    echo "[contrib/freq] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Updating snp_id_current ..."
+    psql $PG_DB $PG_USER -c "UPDATE allelefreq_${source_id} \
+                             SET snp_id = a.rscurrent \
+                             FROM (SELECT rshigh, rscurrent FROM rsmergearch) a \
+                             WHERE allelefreq_${source_id}.snp_id = a.rshigh"
+    psql $PG_DB $PG_USER -c "DELETE FROM allelefreq_${source_id} WHERE snp_id IS NULL"
 
 done;
 
--- contrib/freq/postgresql/schema/create_tables.sql
+++ contrib/freq/postgresql/schema/create_tables.sql
@@ -6,15 +6,13 @@ CREATE TABLE AlleleFreqSource (
        populations  varchar[]   not null,
        genome_build varchar
 );
-INSERT INTO AlleleFreqSource VALUES (1, '1000genomes_phase1', '{CHB,JPT}', 'b37'),
-                                    (2, '1000genomes_phase3', '{CHB,JPT}', 'b37'),
-                                    (3, '1000genomes_phase1', '{CHB,JPT,CHS}', 'b37'),
-                                    (4, '1000genomes_phase3', '{CHB,JPT,CHS}', 'b37'),
-                                    (5, '1000genomes_phase3', '{CHB}', 'b37'),
-                                    (6, '1000genomes_phase3', '{JPT}', 'b37'),
-                                    (100, '1000genomes_phase3', '{CEU}', 'b37'),
-                                    (200, '1000genomes_phase3', '{YRI}', 'b37'),
-                                    (300, '1000genomes_phase3', '{GLOBAL}', 'b37');
+INSERT INTO AlleleFreqSource VALUES (1,   '1000genomes_phase1',  '{JPT}',          'b37'),
+                                    (2,   '1000genomes_phase3',  '{JPT}',          'b37'),
+                                    (3,   '1000genomes_phase1',  '{CHB,JPT,CHS}',  'b37'),
+                                    (4,   '1000genomes_phase3',  '{CHB,JPT,CHS}',  'b37'),
+                                    (100, '1000genomes_phase3',  '{CEU}',          'b37'),
+                                    (200, '1000genomes_phase3',  '{YRI}',          'b37'),
+                                    (300, '1000genomes_phase3',  '{GLOBAL}',       'b37');
 
 -- Partitioning "master" table
 DROP TABLE IF EXISTS AlleleFreq CASCADE;
@@ -26,12 +24,10 @@ CREATE TABLE AlleleFreq (
 );
 
 -- Partitioning "child" table
-CREATE TABLE AlleleFreq_1 ( CHECK ( source_id = 1 ) ) INHERITS (AlleleFreq);
-CREATE TABLE AlleleFreq_2 ( CHECK ( source_id = 2 ) ) INHERITS (AlleleFreq);
-CREATE TABLE AlleleFreq_3 ( CHECK ( source_id = 3 ) ) INHERITS (AlleleFreq);
-CREATE TABLE AlleleFreq_4 ( CHECK ( source_id = 4 ) ) INHERITS (AlleleFreq);
-CREATE TABLE AlleleFreq_5 ( CHECK ( source_id = 5 ) ) INHERITS (AlleleFreq);
-CREATE TABLE AlleleFreq_6 ( CHECK ( source_id = 6 ) ) INHERITS (AlleleFreq);
+CREATE TABLE AlleleFreq_1   ( CHECK ( source_id = 1 )   ) INHERITS (AlleleFreq);
+CREATE TABLE AlleleFreq_2   ( CHECK ( source_id = 2 )   ) INHERITS (AlleleFreq);
+CREATE TABLE AlleleFreq_3   ( CHECK ( source_id = 3 )   ) INHERITS (AlleleFreq);
+CREATE TABLE AlleleFreq_4   ( CHECK ( source_id = 4 )   ) INHERITS (AlleleFreq);
 CREATE TABLE AlleleFreq_100 ( CHECK ( source_id = 100 ) ) INHERITS (AlleleFreq);
 CREATE TABLE AlleleFreq_200 ( CHECK ( source_id = 200 ) ) INHERITS (AlleleFreq);
 CREATE TABLE AlleleFreq_300 ( CHECK ( source_id = 300 ) ) INHERITS (AlleleFreq);
@@ -40,18 +36,14 @@ CREATE TABLE AlleleFreq_300 ( CHECK ( source_id = 300 ) ) INHERITS (AlleleFreq);
 CREATE OR REPLACE FUNCTION allelefreq_insert_trigger()
 RETURNS TRIGGER AS $$
     BEGIN
-        -- IF ( NEW.source_id = 1 ) THEN
-        --     INSERT INTO allelefreq_1 VALUES (NEW.*);
-        IF ( NEW.source_id = 2 ) THEN
+        IF ( NEW.source_id = 1 ) THEN
+            INSERT INTO allelefreq_1 VALUES (NEW.*);
+        ELSIF ( NEW.source_id = 2 ) THEN
             INSERT INTO allelefreq_2 VALUES (NEW.*);
-        -- ELSIF ( NEW.source_id = 3 ) THEN
-        --     INSERT INTO allelefreq_3 VALUES (NEW.*);
-        -- ELSIF ( NEW.source_id = 4 ) THEN
-        --     INSERT INTO allelefreq_4 VALUES (NEW.*);
-        -- ELSIF ( NEW.source_id = 5 ) THEN
-        --     INSERT INTO allelefreq_5 VALUES (NEW.*);
-        -- ELSIF ( NEW.source_id = 6 ) THEN
-        --     INSERT INTO allelefreq_6 VALUES (NEW.*);
+        ELSIF ( NEW.source_id = 3 ) THEN
+            INSERT INTO allelefreq_3 VALUES (NEW.*);
+        ELSIF ( NEW.source_id = 4 ) THEN
+            INSERT INTO allelefreq_4 VALUES (NEW.*);
         ELSIF ( NEW.source_id = 100 ) THEN
             INSERT INTO allelefreq_100 VALUES (NEW.*);
         ELSIF ( NEW.source_id = 200 ) THEN
