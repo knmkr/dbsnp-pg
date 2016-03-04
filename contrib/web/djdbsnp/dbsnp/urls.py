18e6fa1bb9907bcf4a3952ede693b7a6170308ca
Kensuke Numakura <knmkr3gma@gmail.com>
2016-03-04 11:50:15 +0900

    disable unused urls

--- contrib/web/djdbsnp/dbsnp/urls.py
+++ contrib/web/djdbsnp/dbsnp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^snps/$', views.snps, name='snps'),
-    url(r'^snp/(\d+)$', views.snp, name='snp'),
+    # url(r'^snp/(\d+)$', views.snp, name='snp'),
 ]
