c5ccf844d7411485fe1e4b20cd2b1ca1dbae1fbe
Kensuke Numakura <knmkr3gma@gmail.com>
2016-03-08 12:17:30 +0900

    add select box to choice allele freq soruce

--- contrib/web/djdbsnp/dbsnp/forms.py
+++ contrib/web/djdbsnp/dbsnp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
+AF_POPULATION_CHOICES = (
+    (3,   '1000 Genomes Phase1 CHB+JPT+CHS'),
+    (0,   '-------------------------------'),
+    (4,   '1000 Genomes Phase3 CHB+JPT+CHS'),
+    (100, '1000 Genomes Phase3 CEU'),
+    (200, '1000 Genomes Phase3 YRI'),
+    (300, '1000 Genomes Phase3 Global'),
+)
+
 class SnpsForm(forms.Form):
+    af_population = forms.ChoiceField(widget=forms.widgets.Select, choices=AF_POPULATION_CHOICES)
     rsids = forms.CharField(widget=forms.Textarea)
 
     def clean_rsids(self):
