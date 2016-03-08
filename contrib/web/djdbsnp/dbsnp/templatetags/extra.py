9a02e322e22d847d13779202b47ae177a8b88900
Kensuke Numakura <knmkr3gma@gmail.com>
2016-03-08 13:20:29 +0900

    show N/A if not founds

--- contrib/web/djdbsnp/dbsnp/templatetags/extra.py
+++ contrib/web/djdbsnp/dbsnp/templatetags/extra.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def css_class(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+@register.filter
+def na(value):
+    return value if value else 'N/A'
