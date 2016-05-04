6f12d71614dffed152c9e5bcbfbaeeee61df2b53
Kensuke Numakura <knmkr3gma@gmail.com>
2016-05-04 21:46:06 +0900

    update pg_restore

--- script/pg_restore.sh
+++ script/pg_restore.sh
@@ -13,11 +13,13 @@ cd ${RELEASE}
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetchging data ..."
 PG_DUMP=${PG_DB//_/-}-${TAG}.pg_dump  # dbsnp-bxxx-GRChxx-x.x.x.pg_dump
 wget -c https://github.com/knmkr/dbsnp-pg/releases/download/${TAG}/${PG_DUMP}.a{a,b,c,d,e,f,g}
-cat ${PG_DUMP}.a{a,b,c,d,e,f,g} > ${PG_DUMP}
-rm ${PG_DUMP}.a{a,b,c,d,e,f,g}
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_restore ..."
+cat ${PG_DUMP}.a{a,b,c,d,e,f,g} > ${PG_DUMP}
 NUM_JOB=$(getconf _NPROCESSORS_ONLN)
 pg_restore -C -d ${PG_DB} -U ${PG_USER} -j ${NUM_JOB} ${PG_DUMP}
+
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Remove intermediate files"
+rm ${PG_DUMP}.a{a,b,c,d,e,f,g}
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
