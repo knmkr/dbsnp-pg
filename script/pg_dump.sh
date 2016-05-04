54961bc0f256c5f804c310df7a4caa68f41ca638
Kensuke Numakura <knmkr3gma@gmail.com>
2016-05-04 21:04:50 +0900

    update pg_dump and pg_restore

--- script/pg_dump.sh
+++ script/pg_dump.sh
@@ -7,4 +7,6 @@ PG_USER=$2
 TAG=$3
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_dump ..."
-pg_dump -Fc $PG_DB -U $PG_USER > ${PG_DB//_/-}.${TAG}.pg_dump
+pg_dump -Fc $PG_DB -U $PG_USER > ${PG_DB//_/-}-${TAG}.pg_dump
+
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
--- script/pg_restore.sh
+++ script/pg_restore.sh
@@ -6,7 +6,7 @@ PG_DB=$1
 PG_USER=$2
 TAG=$3
 
-PG_DUMP=${PG_DB//_/-}.${TAG}.pg_dump  # replace `_` to `-`
+PG_DUMP=${PG_DB//_/-}-${TAG}.pg_dump  # replace `_` to `-`
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetchging data ..."
 wget -c https://github.com/knmkr/dbsnp-pg/releases/download/${TAG}/${PG_DUMP}.a{a,b,c,d,e}
@@ -16,3 +16,5 @@ rm ${PG_DUMP}.a{a,b,c,d,e}
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_restore ..."
 NUM_JOB=$(getconf _NPROCESSORS_ONLN)
 pg_restore -C -d ${PG_DB} -U ${PG_USER} -j ${NUM_JOB} ${PG_DUMP}
+
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
