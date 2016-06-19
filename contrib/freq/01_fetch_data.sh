e25e261c2b0bc501cf1c049f6bbce6a1f066c301
Kensuke Numakura <knmkr3gma@gmail.com>
2016-06-19 12:21:50 +0900

    revert 1000 genomes phase1 ftp url

--- contrib/freq/01_fetch_data.sh
+++ contrib/freq/01_fetch_data.sh
@@ -10,9 +10,7 @@ echo "[contrib/freq] [INFO] Fetching data..."
 # 1000 genomes phase1
 mkdir -p 1000genomes.phase1
 cd 1000genomes.phase1
-wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr{1..22}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz{,.tbi}
-wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf.gz{,.tbi}
-wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrY.phase3_integrated_v2a.20130502.genotypes.vcf.gz{,.tbi}
+wget -c ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20110521/ALL.chr{{1..22},X}.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz{,.tbi}
 
 cd ..
 
