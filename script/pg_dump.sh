3528fe5d8944e4294bfcef170c9446c966d41885
Kensuke Numakura <knmkr3gma@gmail.com>
2016-07-25 17:31:02 +0900

    update dump script

--- script/pg_dump.sh
+++ script/pg_dump.sh
@@ -6,7 +6,14 @@ PG_DB=$1
 PG_USER=$2
 TAG=$3
 
+NAME=${PG_DB//_/-}-${TAG}
+
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_dump ..."
-pg_dump -Fc $PG_DB -U $PG_USER > ${PG_DB//_/-}-${TAG}.pg_dump
+pg_dump -Fc $PG_DB -U $PG_USER -T 'allelefreq*' > ${NAME}.pg_dump
+pg_dump -Fc $PG_DB -U $PG_USER -t 'allelefreq*' > ${NAME}.freq.pg_dump
+
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` split ..."
+split -b 999m ${NAME}.pg_dump ${NAME}.pg_dump.
+split -b 999m ${NAME}.freq.pg_dump ${NAME}.freq.pg_dump.
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
