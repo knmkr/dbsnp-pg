7f78a28c6ee43ddd3d6281ccb311afcc16261663
Kensuke Numakura <knmkr3gma@gmail.com>
2016-08-09 18:00:48 +0900

    fix settings path issue

--- contrib/web/djdbsnp/djdbsnp/wsgi.py
+++ contrib/web/djdbsnp/djdbsnp/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djdbsnp.settings")
 
 application = get_wsgi_application()
