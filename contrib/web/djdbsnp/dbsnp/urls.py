a1a68c348e4a3cac0d2f80df9d1b8fa4c198b4e1
Kensuke Numakura <knmkr3gma@gmail.com>
2016-08-14 22:18:19 +0900

    rename uri

--- contrib/web/djdbsnp/dbsnp/urls.py
+++ contrib/web/djdbsnp/dbsnp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^snps/$', views.snps, name='snps'),
-    url(r'^snp/(\d{1,9})$', views.snp, name='snp'),  # max rs is 483352819 (b141)
+    url(r'^snps/(\d{1,9})$', views.snp, name='snp'),  # max rs is 483352819 (b141)
 ]
