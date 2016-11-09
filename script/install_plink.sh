43e6775b5688d79d68041af1c9cbfd6f932fb247
Kensuke Numakura <knmkr3gma@gmail.com>
2016-11-09 12:00:20 +0900

    plink installer script

---
+++ script/install_plink.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -ue
+set -o pipefail
+
+BASEDIR=${1:-$(pwd)}
+BINDIR=$BASEDIR/bin
+mkdir -p $BINDIR
+TMPDIR=$BASEDIR/.tmp.install_plink
+mkdir -p $TMPDIR
+
+cd $TMPDIR
+
+case $OSTYPE in
+  darwin*)
+    URL="https://www.cog-genomics.org/static/bin/plink161010/plink_mac.zip"
+    ;;
+  linux*)
+    URL="https://www.cog-genomics.org/static/bin/plink161010/plink_linux_x86_64.zip"
+    ;;
+esac
+
+wget -c $URL -O plink.zip
+unzip plink.zip plink
+cp plink $BINDIR
+
+$BINDIR/plink --version
+
+cd $BASEDIR
+rm -rf $TMPDIR
