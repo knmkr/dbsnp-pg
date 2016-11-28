c9763611e58a7de9d19805f315e0cbc5cb61d251
Kensuke Numakura <knmkr3gma@gmail.com>
2016-11-28 12:23:34 +0900

    json api for /dbsnp/dnps

--- contrib/web/djdbsnp/dbsnp/views.py
+++ contrib/web/djdbsnp/dbsnp/views.py
@@ -1,7 +1,9 @@
 import re
 import csv
+import json
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from .models import SNP
 from .forms import SnpsForm
@@ -10,21 +12,39 @@ from .forms import SnpsForm
 def index(request):
     return redirect(snps)
 
+@csrf_exempt  # FIXME: change to GET?
 def snps(request):
-    form = SnpsForm()
-    context = {'form': form}
+    fmt = request.GET.get('fmt', '')
 
-    if request.method == "POST":
-        form = SnpsForm(request.POST)
-        if form.is_valid():
-            af_population = form.cleaned_data.get('af_population')
-            rsids = form.cleaned_data.get('rsids')
-            af_order = form.cleaned_data.get('af_order')
+    if fmt == 'json':
+        context = {}
+        if request.method == "POST":
+            # FIXME: need to validate params
+            data = json.loads(request.body)
+            af_population = data.get('af_population')
+            rsids = data.get('rsids')
+            af_order = data.get('af_order')
+
             context['allele_freqs'] = SNP.get_allele_freqs(af_population, rsids, af_order)
             context['chr_pos'] = SNP.get_pos_by_rs(rsids)
-        context['form'] = form
+        response = JsonResponse(context)
 
-    return render(request, 'snps.html', context)
+        return response
+
+    else:
+        form = SnpsForm()
+        context = {'form': form}
+        if request.method == "POST":
+            form = SnpsForm(request.POST)
+            if form.is_valid():
+                af_population = form.cleaned_data.get('af_population')
+                rsids = form.cleaned_data.get('rsids')
+                af_order = form.cleaned_data.get('af_order')
+                context['allele_freqs'] = SNP.get_allele_freqs(af_population, rsids, af_order)
+                context['chr_pos'] = SNP.get_pos_by_rs(rsids)
+            context['form'] = form
+
+        return render(request, 'snps.html', context)
 
 def snp(request, rsid):
     context = {
