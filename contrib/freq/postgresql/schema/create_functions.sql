a5520afec13bc67d1370ae98bebecb4aa76ef491
Kensuke Numakura <knmkr3gma@gmail.com>
2016-12-07 10:11:19 +0900

    include reference allele in query results

--- contrib/freq/postgresql/schema/create_functions.sql
+++ contrib/freq/postgresql/schema/create_functions.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE FUNCTION get_allele_freq(
   _rs int[],
   OUT snp_id int,
   OUT snp_current int,
+  OUT "ref" varchar,
   OUT allele varchar[],
   OUT freq real[],
   OUT freqx integer[]
@@ -13,12 +14,13 @@ BEGIN
       SELECT
           a.snp_id,
           a.snp_current,
+          f.ref,
           f.allele,
           f.freq,
           f.freqx
       FROM
           get_current_rs(_rs) a
-          LEFT OUTER JOIN (SELECT af.snp_id, af.allele, af.freq, af.freqx FROM allelefreq af WHERE source_id = _source_id) f ON a.snp_current = f.snp_id  -- f.snp_id have been updated to current while bulk importing
+          LEFT OUTER JOIN (SELECT af.snp_id, af.ref, af.allele, af.freq, af.freqx FROM allelefreq af WHERE source_id = _source_id) f ON a.snp_current = f.snp_id  -- f.snp_id have been updated to current while bulk importing
   );
 END
 $$ LANGUAGE plpgsql;
