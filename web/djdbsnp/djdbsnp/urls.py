043c62b27a6442cff0d5c4129e1da06e067c46fb
Kensuke Numakura <knmkr3gma@gmail.com>
2016-12-12 18:02:28 +0900

    API schema

--- web/djdbsnp/djdbsnp/urls.py
+++ web/djdbsnp/djdbsnp/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
 
     url(r'^', include('dbsnp.urls')),
+    url('^schema/$', schema_view),
 ]
