a2d06de85c1c3d085d7b45356ab3c998dad6f023
Kensuke Numakura <knmkr3gma@gmail.com>
2017-02-24 16:13:07 +0900

    cleanup variable names

--- script/pg_restore.sh
+++ script/pg_restore.sh
@@ -2,25 +2,35 @@
 
 set -eu
 
-PG_DB_SRC=$1
-PG_DB_DST=$2
-PG_USER=$3
-TAG=$4
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <PG_DB> <PG_USER> <TAG> <DBSNP_BUILD> <REF_BUILD>" >&2
+    exit 1
+fi
 
-RELEASE=release/${TAG}/${PG_DB_SRC/dbsnp_/}  # release/x.x.x/bxxx_GRChxx/
-mkdir -p ${RELEASE}
-cd ${RELEASE}
+PG_DB=$1
+PG_USER=$2
+TAG=$3
+DBSNP_BUILD=$4
+REF_BUILD=$5
 
-echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetchging data ..."
-PG_DUMP=${PG_DB_SRC//_/-}-${TAG}.pg_dump  # dbsnp-bxxx-GRChxx-x.x.x.pg_dump
-wget -c https://github.com/knmkr/dbsnp-pg/releases/download/${TAG}/${PG_DUMP}.a{a,b,c,d,e,f,g}
+PG_DUMP=dbsnp-${DBSNP_BUILD}-${REF_BUILD}-${TAG}.pg_dump
+CHUNKS=$(echo ${PG_DUMP}.a{a,b,c,d,e,f,g,h,i})
 
-echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_restore ..."
-cat ${PG_DUMP}.a{a,b,c,d,e,f,g} > ${PG_DUMP}
+if [ ! -f ${PG_DUMP} ]; then
+    echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetchging data..."
+    for chunk in $CHUNKS; do
+        wget -c https://github.com/knmkr/dbsnp-pg/releases/download/${TAG}/${chunk};
+    done
+
+    echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Concatenating chunks..."
+    cat ${CHUNKS} > ${PG_DUMP}
+fi
+
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` pg_restore..."
 NUM_JOB=$(getconf _NPROCESSORS_ONLN)
-pg_restore -v --clean --if-exists -n public -d ${PG_DB_DST} -U ${PG_USER} -j ${NUM_JOB} ${PG_DUMP}
+pg_restore -v --clean --if-exists -n public -d ${PG_DB} -U ${PG_USER} -j ${NUM_JOB} ${PG_DUMP}
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Remove intermediate files"
-rm ${PG_DUMP}.a{a,b,c,d,e,f,g}
+rm -f ${CHUNKS}
 
 echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
