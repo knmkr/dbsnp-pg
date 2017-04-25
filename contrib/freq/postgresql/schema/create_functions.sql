14dcd25da0eb0ea823fa058091bc4b08fd590fd9
Kensuke Numakura <knmkr3gma@gmail.com>
2017-04-25 14:20:13 +0900

    fix ordinality of duplicate records

--- contrib/freq/postgresql/schema/create_functions.sql
+++ contrib/freq/postgresql/schema/create_functions.sql
@@ -12,16 +12,28 @@ CREATE OR REPLACE FUNCTION get_allele_freq(
 BEGIN
   RETURN QUERY (
       SELECT
-          DISTINCT
-          a.snp_id,
-          a.snp_current,
-          f.ref,
-          f.allele,
-          f.freq,
-          f.freqx
+          s.snp_id,
+          s.snp_current,
+          s.ref,
+          s.allele,
+          s.freq,
+          s.freqx
       FROM
-          get_current_rs(_rs) a
-          LEFT OUTER JOIN (SELECT af.snp_id, af.ref, af.allele, af.freq, af.freqx FROM allelefreq af WHERE source_id = _source_id) f ON a.snp_current = f.snp_id  -- f.snp_id have been updated to current while bulk importing
-  );
+          (SELECT DISTINCT
+              q.ordinality,
+              a.snp_id,
+              a.snp_current,
+              f.ref,
+              f.allele,
+              f.freq,
+              f.freqx
+          FROM
+              (SELECT * FROM unnest(_rs) WITH ORDINALITY snp_id) q  -- v9.4 or later
+              JOIN get_current_rs(_rs) a USING (snp_id)
+              LEFT OUTER JOIN (SELECT af.snp_id, af.ref, af.allele, af.freq, af.freqx FROM allelefreq af WHERE source_id = _source_id) f ON a.snp_current = f.snp_id  -- f.snp_id have been updated to current while bulk importing
+          ) s
+      ORDER BY
+          s.ordinality
+);
 END
 $$ LANGUAGE plpgsql;
